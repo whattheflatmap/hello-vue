a9fb08de2c43bb08aa4c9789917d89fdb5387499
Mohammed Azmi <mohammed.azmi@cdl.com.sg>
2024-07-31 22:50:31 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
